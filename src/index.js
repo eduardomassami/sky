eff2b2b96a9b6ea012938bb4808e8aa909294a0e
Eduardo Massami Uchida <massami.uchida@gmail.com>
2019-06-28 02:15:29 -0300

    teste deploy

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const serverPort = process.env.PORT || 3000
 app.listen({ port: serverPort }, () => console.log(`Server listening port ${serverPort}.`));
 
 app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/user', user);
 
